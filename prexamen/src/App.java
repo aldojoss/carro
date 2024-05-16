e264d1e3d7265d4ba84d7fd27f07cfdfe5557ba3
aldojoss <168256201+aldojoss@users.noreply.github.com>
2024-05-15 22:12:41 -0600

    Update App.java

--- prexamen/src/App.java
+++ prexamen/src/App.java
@@ -5,5 +5,13 @@ public class App {
     Vehiculo carro =new Vehiculo(null, null, 0, 0);
 carro.opciones();
 
+        System.out.println("La marca de su carro es "+ carro.getMarca());
+ System.out.println("------------------------------------------------"); 
+ System.out.println("El nivel de gasolina que conserva es de "+ carro.getNivelactualcombustible()); 
+ System.out.println("------------------------------------------------"); 
+ System.out.println("Su capacidad maxima es de "+ carro.getCapacidadtotaldeltanque()); 
+ System.out.println("------------------------------------------------"); 
+ System.out.println(" Su numero de placa es de "+ carro.getNumerodeplaca()); 
+
     }
 }
